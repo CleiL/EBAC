0e989e106b74335359d52ff897314678507d8384
Clei Lisboa <64207875+CleiL@users.noreply.github.com>
2023-04-03 14:42:43 -0300

    Add files via upload

---
+++ teste.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicar(a, b) {
+    return a * b;
+}
+function saudacao(nome) {
+    return `Olá ${nome}`;
+}
+console.log(multiplicar(10,20));
+console.log(saudacao('Clei'));
